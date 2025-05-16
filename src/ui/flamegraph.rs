c3c0cb51e7553ee7a53a2766a7c63d1447d6b849
Islam Bekbuzarov <n157318@gmail.com>
2025-05-16 23:04:42 +0300

    added a rough version of flamegraph

--- src/ui/flamegraph.rs
+++ src/ui/flamegraph.rs
@@ -1,33 +1,67 @@
-use crate::ui::widgets::flamegraph::{draw_flamegraph, Options, StackFrame};
+use crate::ui::widgets::flamegraph::{draw_flamegraph, Options};
 use crate::ui::MemInfo;
+use common::parser::{AccumulatedData, Frame, InstructionPointer};
 use egui::Ui;
+use std::iter;
+
+struct Line {
+    frames: Vec<String>,
+    value: f64,
+}
+
+impl Line {
+    pub fn new(value: f64) -> Self {
+        Self {
+            frames: Vec::new(),
+            value,
+        }
+    }
+
+    fn into_string(self) -> String {
+        let frames = self
+            .frames
+            .into_iter()
+            .rev()
+            .collect::<Vec<String>>()
+            .join(";");
+
+        format!("{} {}", frames, self.value)
+    }
+}
 
 pub fn show_ui(ui: &mut Ui, info: &MemInfo) {
     let options = Options { frame_height: 20.0 };
+    let mut lines = Vec::new();
 
-    let root = [
-        StackFrame {
-            label: "main".to_string(),
-            value: 5.0,
-            children: vec![
-                StackFrame {
-                    label: "ch1".to_string(),
-                    value: 3.0,
-                    children: vec![],
-                },
-                StackFrame {
-                    label: "ch2".to_string(),
-                    value: 2.0,
-                    children: vec![],
-                },
-            ],
-        },
-        StackFrame {
-            label: "side".to_string(),
-            value: 7.0,
-            children: vec![],
-        },
-    ];
+    for alloc_info in &info.data.allocation_infos {
+        let allocation = &info.data.allocations[alloc_info.allocation_idx as usize];
+        let mut trace_idx = allocation.trace_idx;
 
-    draw_flamegraph(ui, options, &root);
+        let mut line = Line::new(alloc_info.size as f64);
+        while trace_idx != 0 {
+            let trace = &info.data.traces[trace_idx as usize - 1];
+            let ip_info = &info.data.instruction_pointers[trace.ip_idx as usize - 1];
+
+            let frames = get_frames_from_ip_info(&info.data, ip_info);
+            line.frames.extend(frames);
+
+            trace_idx = trace.parent_idx;
+        }
+        lines.push(line.into_string());
+    }
+
+    draw_flamegraph(ui, options, lines.iter().map(|v| v.as_str()));
+}
+
+fn get_frames_from_ip_info(data: &AccumulatedData, ip_info: &InstructionPointer) -> Vec<String> {
+    iter::once(&ip_info.frame)
+        .chain(ip_info.inlined.iter())
+        .map(|frame| {
+            let function_idx = match frame {
+                Frame::Single { function_idx } => function_idx,
+                Frame::Multiple { function_idx, .. } => function_idx,
+            };
+            data.strings[function_idx - 1].clone()
+        })
+        .collect()
 }
--- src/ui/widgets/flamegraph.rs
+++ src/ui/widgets/flamegraph.rs
@@ -1,4 +1,5 @@
 use eframe::egui::*;
+use std::collections::BTreeMap;
 
 const FRAME_V_SPACING: f32 = 4.0;
 const FRAME_H_SPACING: f32 = 4.0;
@@ -9,11 +10,11 @@ pub struct Options {
     pub frame_height: f32,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct StackFrame {
     pub label: String,
     pub value: f64,
-    pub children: Vec<StackFrame>,
+    pub children: BTreeMap<String, StackFrame>,
 }
 
 struct Canvas {
@@ -23,7 +24,13 @@ struct Canvas {
     options: Options,
 }
 
-pub fn draw_flamegraph(ui: &mut Ui, options: Options, root_frames: &[StackFrame]) {
+pub fn draw_flamegraph<'a>(
+    ui: &mut Ui,
+    options: Options,
+    frames: impl IntoIterator<Item = &'a str>,
+) {
+    let root_frame = build_stackframes(frames);
+
     ui.horizontal_centered(|ui| {
         Frame::canvas(ui.style()).show(ui, |ui| {
             let rect = ui.available_rect_before_wrap();
@@ -35,70 +42,84 @@ pub fn draw_flamegraph(ui: &mut Ui, options: Options, root_frames: &[StackFrame]
                 options,
             };
 
-            draw_root_frames(&canvas, root_frames);
+            draw_root_frames(&canvas, root_frame);
         });
     });
 }
 
-fn draw_root_frames(canvas: &Canvas, frames: &[StackFrame]) {
+fn draw_root_frames(canvas: &Canvas, root: StackFrame) {
     let min_x = canvas.rect.min.x;
     let max_x = canvas.rect.max.x;
 
-    let root = StackFrame {
-        label: "".to_string(),
-        value: frames.iter().map(|frame| frame.value).sum(),
-        children: frames.to_vec(),
-    };
-
-    draw_one_frame(canvas, &root, 0, min_x, max_x, true);
+    draw_one_frame(canvas, &root, 0, min_x, max_x);
 }
 
-fn draw_one_frame(
-    canvas: &Canvas,
-    frame: &StackFrame,
-    depht: u32,
-    min_x: f32,
-    max_x: f32,
-    first_run: bool,
-) {
-    let min_y = if first_run {
-        canvas.rect.min.y
-    } else {
-        canvas.rect.min.y + (depht - 1) as f32 * (canvas.options.frame_height + FRAME_V_SPACING)
-    };
+fn draw_one_frame(canvas: &Canvas, frame: &StackFrame, depht: u32, min_x: f32, max_x: f32) {
+    let min_y = canvas.rect.min.y + depht as f32 * (canvas.options.frame_height + FRAME_V_SPACING);
 
     let max_y = min_y + canvas.options.frame_height;
 
-    if !first_run {
-        let rect = Rect::from_min_max(pos2(min_x, min_y), pos2(max_x, max_y));
+    let rect = Rect::from_min_max(pos2(min_x, min_y), pos2(max_x, max_y));
 
-        canvas.painter.rect_filled(rect, 0.0, Color32::GREEN);
-        let painter = canvas.painter.with_clip_rect(rect.intersect(canvas.rect));
-        let text = format!("{}: {}", frame.label, frame.value);
+    canvas.painter.rect_filled(rect, 0.0, Color32::GREEN);
+    let painter = canvas.painter.with_clip_rect(rect.intersect(canvas.rect));
+    let text = format!("{}: {}", frame.label, frame.value);
 
-        let text_pos = pos2(
-            min_x + 4.0,
-            min_y + 0.5 * (canvas.options.frame_height - TEXT_HEIGHT),
-        );
+    let text_pos = pos2(
+        min_x + 4.0,
+        min_y + 0.5 * (canvas.options.frame_height - TEXT_HEIGHT),
+    );
 
-        painter.text(
-            text_pos,
-            Align2::LEFT_TOP,
-            text,
-            FontId::default(),
-            Color32::BLACK,
-        );
-    }
+    painter.text(
+        text_pos,
+        Align2::LEFT_TOP,
+        text,
+        FontId::default(),
+        Color32::BLACK,
+    );
 
     let mut child_min_x = min_x;
     let length = max_x - min_x;
-    for child in &frame.children {
+    for (_, child) in &frame.children {
         let child_value = frame.value.min(child.value);
 
         let child_max_x = max_x.min(child_min_x + (child_value / frame.value) as f32 * length);
 
-        draw_one_frame(canvas, child, depht + 1, child_min_x, child_max_x, false);
+        draw_one_frame(canvas, child, depht + 1, child_min_x, child_max_x);
 
         child_min_x = child_max_x + FRAME_H_SPACING;
     }
+}
+
+fn build_stackframes<'a>(chains: impl IntoIterator<Item = &'a str>) -> StackFrame {
+    let mut root = StackFrame::default();
+    root.label = "all".to_string();
+
+    for chain in chains {
+        let (frames, value) = chain.rsplit_once(" ").unwrap();
+        let value = value.parse::<f64>().unwrap();
+
+        root.value += value;
+        fill_children(&mut root, frames, value);
+    }
+
+    root
+}
+
+fn fill_children(sf: &mut StackFrame, frames: &str, value: f64) {
+    let Some((frame, frames)) = frames.split_once(";") else {
+        sf.label = frames.to_string();
+        sf.value = value;
+        return;
+    };
+
+    let next = sf.children.entry(frame.to_string()).or_insert(StackFrame {
+        label: frame.to_string(),
+        value: 0.0,
+        children: Default::default(),
+    });
+
+    next.value += value;
+
+    fill_children(next, frames, value);
 }
